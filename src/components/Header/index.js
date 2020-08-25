be8651e2ecff7730fcabdc3e45f1410c44c17f14
wrgraff <wrgraff@gmail.com>
2020-08-26 02:41:08 +0300

    Создает базовые настройки

---
+++ src/components/Header/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './index.scss';
+
+const Header = () => {
+    return (
+        <header className="header">
+            <h1 className="header__heading">Редактирование расписания занятий</h1>
+            <div className="header__buttons">
+                
+            </div>
+        </header>
+    );
+};
+
+export default Header;
