ee737cc4d124a8fdf1a9d4ef4b5a6cf3e8077f75
Artur Trifonov <wrgraff@gmail.com>
2020-12-27 19:31:00 +0300

    update add session reducer

--- src/reducers/sessionsReducer.js
+++ src/reducers/sessionsReducer.js
@@ -3,8 +3,10 @@ import _ from 'lodash';
 export default (state = {}, action) => {
     switch (action.type) {
         case 'FETCH_SESSIONS':
-        case 'ADD_SESSION':
             return { ...state, ..._.mapKeys(action.payload, 'id')};
+
+        case 'ADD_SESSION':
+            return { ...state, [action.payload.id]: action.payload};
 
         default:
             return state;
